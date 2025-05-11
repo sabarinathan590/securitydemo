551b91b03e699457253da7980edbe4ff13429256
Sabarinathan S <sabarinathans@dlt.sg>
2025-05-11 14:35:17 +0530

    inMemory authentication added

--- src/main/java/com/example/securitydemo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/securitydemo/configuration/SecurityConfiguration.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,4 +22,10 @@ public class SecurityConfiguration {
         http.httpBasic(withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService initialiseUsers(){
+        return new InMemoryUserDetailsManager(User.builder().username("user1").password("{noop}user1").roles("USER").build());
+    }
 }
+
