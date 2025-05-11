39bde22197cf7a93eae6797e1e01ffceb5457b09
Sabarinathan S <sabarinathans@dlt.sg>
2025-05-11 14:11:42 +0530

    enabled http basic authentication and disabled form based authentication

---
+++ src/main/java/com/example/securitydemo/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.securitydemo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
