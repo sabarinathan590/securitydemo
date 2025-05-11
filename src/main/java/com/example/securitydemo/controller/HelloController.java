407cdaa44cfac0b3d1a6d18770aeed01973a00a5
Sabarinathan S <sabarinathans@dlt.sg>
2025-05-11 16:35:43 +0530

    method security using pre authorize

--- src/main/java/com/example/securitydemo/controller/HelloController.java
+++ src/main/java/com/example/securitydemo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.securitydemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,17 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String helloUser(){
+        return "Hello, User!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String helloAdmin(){
+        return "Hello, Admin!";
     }
 }
